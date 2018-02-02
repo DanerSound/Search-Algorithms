2931bcfd2a82784501a332b69026a5b443c6e34c
DanerSound <andre.barreto28@gmail.com>
2018-02-02 17:11:19 +0100

    complete first algorithm

--- Search-algorithms/src/base/SequentialSort.java
+++ Search-algorithms/src/base/SequentialSort.java
@@ -8,12 +8,12 @@ public class SequentialSort {
 	   
 	  for(i=0;i<=v.length-2;i++) { 
 	    min=i; 
-	    for(j=i+1;i<v.length-1;j++) { 
+	    for(j=i+1;j<v.length-1;j++) { 
 	      if(v[j]<v[min]) 
 	        min=j; 
 	    } 
 	  tmp=v[min]; 
-	  v[min]=v[j]; 
+	  v[min]=v[i]; 
 	  v[i]=tmp; 
 	     
 	  } 
