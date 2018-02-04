77a03698b07b7ca2e919e2962107f108a46118ac
DanerSound <andre.barreto28@gmail.com>
2018-02-04 17:07:55 +0100

    fixed space and little rewrite

---
+++ Search-algorithms/src/base/SelectionSort.java
@@ -0,0 +1,20 @@
+package base;
+
+public class SelectionSort {
+
+  static int i, j, min, tmp;
+
+  static void sorting(int[] v) {
+
+  for (i = 0; i <= v.length - 2; i++) {
+		min = i;
+		for (j = i + 1; j < v.length - 1; j++) {
+			if (v[j] < v[min])
+				min = j;
+			}
+		tmp = v[min];
+		v[min] = v[i];
+		v[i] = tmp;
+		}
+	}
+}
