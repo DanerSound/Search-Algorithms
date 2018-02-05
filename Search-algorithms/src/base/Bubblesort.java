b3a7a58393770f68d1cb044f47d7c068323938ac
DanerSound <andre.barreto28@gmail.com>
2018-02-05 18:24:43 +0100

    fixed brackets errors all working

--- Search-algorithms/src/base/Bubblesort.java
+++ Search-algorithms/src/base/Bubblesort.java
@@ -6,12 +6,13 @@ public static int i,j,tmp;
 	
 public static void sorting(int[]v){
 		
-		for(i=n-1;i>=1;i--){
+		for(i=v.length-1;i>=1;i--){
 			for(j=1;j<=i;j++){
-				if(v[j-1]>v[j])
+				if(v[j-1]>v[j]) {
 					tmp=v[j-1];
 					v[j-1]=v[j];
-					v[j]=tmp
+					v[j]=tmp;
+				}
 			}   
 		}
 	}
