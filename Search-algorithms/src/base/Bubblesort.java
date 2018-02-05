f8702f1dc3130d0a1e0256d69610544f3107829f
DanerSound <andre.barreto28@gmail.com>
2018-02-05 17:45:03 +0100

    first try

---
+++ Search-algorithms/src/base/Bubblesort.java
@@ -0,0 +1,18 @@
+package base;
+
+public class Bubblesort {
+	
+public static int i,j,tmp;
+	
+public static void sorting(int[]v){
+		
+		for(i=n-1;i>=1;i--){
+			for(j=1;j<=i;j++){
+				if(v[j-1]>v[j])
+					tmp=v[j-1];
+					v[j-1]=v[j];
+					v[j]=tmp
+			}   
+		}
+	}
+  }
