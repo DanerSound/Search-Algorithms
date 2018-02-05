81f2aa3a7e80f210608fac0c165bc6fba6f9ed62
GabrielePuliti <gabriele.puliti@gmail.com>
2018-02-05 16:25:41 +0100

    create a superclass "sorterAlgorithm" + refactoring

--- Search-algorithms/src/base/InsertionSort.java
+++ Search-algorithms/src/base/InsertionSort.java
@@ -1,18 +1,18 @@
 package base;
 
-public class InsertionSort {
+public class InsertionSort extends SorterAlgorithm {
 	
-	static int i, j, max;
-
-	static void sorting(int[] v) {
-		for (i = 1; i < v.length; i++) {
-			max = v[i];
-			j = i;
+	@Override
+	public int[] sorting(int[] v) {
+		for (int i = 1; i < v.length; i++) {
+			int max = v[i];
+			int j = i;
 			while (j - 1 >= 0 && v[j - 1] > max) {
 				v[j] = v[j - 1];
 				j = j - 1;
 			}
 			v[j] = max;
 		}
+		return v;
 	}
 }
