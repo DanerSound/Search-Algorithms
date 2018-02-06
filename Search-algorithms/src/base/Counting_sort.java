c1f194afda84294fe18fd246e3029e51d85c21b7
DanerSound <andre.barreto28@gmail.com>
2018-02-06 16:09:52 +0100

    not tested alg

---
+++ Search-algorithms/src/base/Counting_sort.java
@@ -0,0 +1,27 @@
+package base;
+
+public class Counting_sort {
+	
+	public static void sorting(int v[],int def[],int maxValue){
+		int[] supp = new int[maxValue];
+		
+		for(int  i=0;i<maxValue;i++){
+			supp[i]=0;
+		}
+		for(int j=1;j<v.length;j++){
+			supp[v[j]]=supp[v[j]]+1;
+		}
+		for(int i=1;i<maxValue;i++){
+			supp[i]=supp[i]+supp[i-1];
+		}
+		
+		for(int j=v.length;j>=1;j--){
+			def[supp[v[j]]]=v[j];
+			supp[v[j]]=supp[v[j]]-1;			
+		}
+		
+		
+	}
+	
+
+}
