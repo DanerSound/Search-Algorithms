5542efa53238306c5a187cd84acfb330d33a1d0e
DanerSound <andre.barreto28@gmail.com>
2018-02-08 17:54:26 +0100

    fixed bug added explaing comment

--- Search-algorithms/src/base/BubblesortFlaged.java
+++ Search-algorithms/src/base/BubblesortFlaged.java
@@ -1,16 +1,20 @@
 package base;
 
-public class BubblesortFlaged {
-	
+public class BubblesortFlaged {	
+	/**
+	 * it will order the array v not view
+	 * @param v messy array
+	 * 
+	 * */	
 	public static void sorting(int v[]) {
 		int i,j,tmp;
-		i=v.length;
+		i=v.length;	
 		boolean sorted=false;
 		
 		while((i>=1)&&(!sorted)){
 			sorted=true;
-			for(j=1;j<=i;j++){
-				if(((j-1)>=0) && (v[j-1]>v[j])){
+			for(j=1;j<i;j++){
+				if(v[j-1]>v[j]){
 				   tmp=v[j-1];
 				   v[j-1]=v[j];
 				   v[j]=tmp;
