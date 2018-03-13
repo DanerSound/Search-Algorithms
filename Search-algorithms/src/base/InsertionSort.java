e76f535549171b497f09b5b8378cb751798fd43c
Wabri <gabriele.puliti@gmail.com>
2018-03-13 16:56:10 +0100

    Done InsertionSort with extremes

--- Search-algorithms/src/base/InsertionSort.java
+++ Search-algorithms/src/base/InsertionSort.java
@@ -1,27 +1,30 @@
 package base;
 
 public class InsertionSort extends SorterAlgorithm {
+
 	/**
 	 * Returns an ordered array of integer elements
 	 * 
-	 * @param arrayToBeOrdered messy array
+	 * @param arrayToBeOrdered
+	 *            messy array
 	 * @return array ordered
 	 */
 	@Override
 	public Integer[] sorting(Integer[] arrayToBeOrdered) {
-		for (int i = 1; i < arrayToBeOrdered.length; i++) {
+		return sorting(arrayToBeOrdered, 0, arrayToBeOrdered.length - 1);
+	}
+
+	@Override
+	public Integer[] sorting(Integer[] arrayToBeOrdered, Integer firstElement, Integer lastElement) {
+		for (int i = firstElement + 1; i < lastElement + 1; i++) {
 			int max = arrayToBeOrdered[i];
 			int j = i;
-			while (j - 1 >= 0 && arrayToBeOrdered[j - 1] > max) {
+			while (j - 1 >= firstElement && arrayToBeOrdered[j - 1] > max) {
 				arrayToBeOrdered[j] = arrayToBeOrdered[j - 1];
 				j = j - 1;
 			}
 			arrayToBeOrdered[j] = max;
 		}
 		return arrayToBeOrdered;
-	}
-	@Override
-	public Integer[] sorting(Integer[] arrayToBeOrdered, Integer firstElement, Integer lastElement) {
-		return sorting(arrayToBeOrdered);
 	}
 }
