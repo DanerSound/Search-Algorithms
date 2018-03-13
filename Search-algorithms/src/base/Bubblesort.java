ee07ca3a1b5925a93fedfc229808bddd2f153535
Wabri <gabriele.puliti@gmail.com>
2018-03-13 15:17:45 +0100

    done refactorization of sorting(Integer[]) method using sorting(Integer[],int,int)

--- Search-algorithms/src/base/Bubblesort.java
+++ Search-algorithms/src/base/Bubblesort.java
@@ -4,16 +4,7 @@ public class Bubblesort extends SorterAlgorithm {
 
 	@Override
 	public Integer[] sorting(Integer[] arrayToBeOrdered) {
-		for (int i = arrayToBeOrdered.length - 1; i >= 1; i--) {
-			for (int j = 1; j <= i; j++) {
-				if (arrayToBeOrdered[j - 1] > arrayToBeOrdered[j]) {
-					int tmp = arrayToBeOrdered[j - 1];
-					arrayToBeOrdered[j - 1] = arrayToBeOrdered[j];
-					arrayToBeOrdered[j] = tmp;
-				}
-			}
-		}
-		return arrayToBeOrdered;
+		return sorting(arrayToBeOrdered,0,arrayToBeOrdered.length-1);
 	}
 
 	@Override
