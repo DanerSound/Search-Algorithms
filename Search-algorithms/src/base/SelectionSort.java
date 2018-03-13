6672052e2b17d6f8c1a2facb57f2e0fd5712562b
Wabri <gabriele.puliti@gmail.com>
2018-03-13 17:19:42 +0100

    bug solved

--- Search-algorithms/src/base/SelectionSort.java
+++ Search-algorithms/src/base/SelectionSort.java
@@ -1,12 +1,17 @@
 package base;
 
 public class SelectionSort extends SorterAlgorithm {
-	
+
 	@Override
 	public Integer[] sorting(Integer[] arrayToBeOrdered) {
-		for (int i = 0; i <= arrayToBeOrdered.length - 2; i++) {
+		return sorting(arrayToBeOrdered, 0, arrayToBeOrdered.length - 1);
+	}
+
+	@Override
+	public Integer[] sorting(Integer[] arrayToBeOrdered, Integer firstElement, Integer lastElement) {
+		for (int i = firstElement; i <= lastElement; i++) {
 			int min = i;
-			for (int j = i + 1; j < arrayToBeOrdered.length - 1; j++) {
+			for (int j = i + 1; j <= lastElement; j++) {
 				if (arrayToBeOrdered[j] < arrayToBeOrdered[min])
 					min = j;
 			}
@@ -15,11 +20,6 @@ public class SelectionSort extends SorterAlgorithm {
 			arrayToBeOrdered[i] = tmp;
 		}
 		return arrayToBeOrdered;
-	}
-
-	@Override
-	public Integer[] sorting(Integer[] arrayToBeOrdered, Integer firstElement, Integer lastElement) {
-		return sorting(arrayToBeOrdered);
 	}
 
 }
