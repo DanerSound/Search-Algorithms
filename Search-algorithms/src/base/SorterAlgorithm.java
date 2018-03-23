23a87fc5258654d96f5d45b4a66293b2b287688a
Wabri <gabriele.puliti@gmail.com>
2018-03-23 18:56:01 +0100

    remove dir

--- Search-algorithms/src/base/SorterAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-public abstract class SorterAlgorithm {
-
-	public abstract Integer[] sorting(Integer[] arrayToBeOrdered);
-  
-	public abstract Integer[] sorting(Integer[] arrayToBeOrdered, Integer firstElement,Integer lastElement);
-
-}
